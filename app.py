062e692b4015c0ddc7624f1e824dd5bcf046b4d2
JITESH PANDURANG SHEWALE <57071101+Jitesh001@users.noreply.github.com>
2020-05-24 18:42:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+
+from flask import Flask, render_template, request
+
+from main import get_record
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/recommend")
+def recommend():
+    movie = request.args.get('movie')
+    r = get_record(movie)
+    movie = movie.upper()
+    if type(r)==type('string'):
+        return render_template('recommend.html',movie=movie,r=r,t='s')
+    else:
+        return render_template('recommend.html',movie=movie,r=r,t='l')
+
+
+
+if __name__ == '__main__':
+    app.run()
